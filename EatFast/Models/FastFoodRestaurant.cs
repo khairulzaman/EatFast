db4ddaef1e47595e8c780f7e80d339b0ef0a9bdd
Khairulzaman Kamarulzaman <khairulzaman.kamarulzaman@gmail.com>
2011-01-23 15:55:18 +0800

    Test commit.

---
+++ EatFast/Models/FastFoodRestaurant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EatFast.Models
+{
+    public class FastFoodRestaurant
+    {
+        public string Name;
+        public decimal lowerPrice;
+        public decimal upperPrice;
+    }
+}
